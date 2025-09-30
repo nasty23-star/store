dc40f9ac51766afeed0e4cc6ee18bb9613a3914b
Anastasia Yarosh <fysyad@gmail.com>
2025-09-30 12:28:28 +0300

    Удалила ненужный импорт

--- src/views/StoreView.vue
+++ src/views/StoreView.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import TheCard from '@/components/TheCard.vue'
-const value = ref('')
 
+const value = ref('')
+const tabs = ref([
+  { id: 'All', label: 'Всё', sortKey: 'name' },
+  { id: 'Auction', label: 'Аукцион', sortKey: 'name' },
+  { id: 'Sales', label: 'Прямые продажи', sortKey: 'name' },
+])
 </script>
 
 <template>
   <div class="container">
-    <TheCard></TheCard>
+    {{ tabs }}
+    <div class="tabs">
+      <button v-for="tab in tabs" :key="tab.id"></button>
+    </div>
+
+    <!-- <TheCard></TheCard> -->
   </div>
 </template>
 
